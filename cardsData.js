7cea005abf29af67f02b30440244e1e2527bfa0f
Amar Besirovic <abesirovic573@gmail.com>
2024-09-24 22:34:31 +0200

    correct data set

---
+++ cardsData.js
@@ -0,0 +1,174 @@
+const cardsData = {
+    1: [ // Phase 1 Data
+        { 
+            price: "$5K", 
+            actualPrice: "$69.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=fc1dc79c-6341-4823-9352-0cec3dda1db2" 
+        },
+        { 
+            price: "$10K", 
+            actualPrice: "$129.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=569854f4-b8dd-4a8b-bb11-7297c9dc1fc9" 
+        },
+        { 
+            price: "$25K", 
+            actualPrice: "$259.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=bccdad21-8585-4fbd-89ef-255e676d3381" 
+        },
+        { 
+            price: "$50K", 
+            actualPrice: "$319.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=f50aef31-54fb-41c9-aa13-68339e71a4d1" 
+        },
+        { 
+            price: "$100K", 
+            actualPrice: "$529.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=9db23e59-1a5d-4bb3-93de-d4679abd9c65" 
+        },
+        { 
+            price: "$200K", 
+            actualPrice: "$989.00", 
+            features: {
+                "Profit Target": { phase1: "10%", funded: "-" },
+                "Duration": { phase1: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "5 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "4%", funded: "4%" },
+                "Max Loss": { phase1: "7%", funded: "7%" },
+                "Max Lots": { phase1: "-", funded: "-" },
+                "Consistency Rule": { phase1: "60%", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=38b403cf-6160-4bef-9904-34cf1ecd4e17" 
+        }
+    ],
+    2: [ // Phase 2 Data
+        { 
+            price: "$5K", 
+            actualPrice: "$69.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=f8157b9c-2ce2-4696-9799-6793ca79f7ae" 
+        },
+        { 
+            price: "$10K", 
+            actualPrice: "$129.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=0353ff34-2c0c-4088-b149-b04d5366672f" 
+        },
+        { 
+            price: "$25K", 
+            actualPrice: "$259.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=4d562b81-e034-4cec-bf42-b070796f42be" 
+        },
+        { 
+            price: "$50K", 
+            actualPrice: "$319.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=813f9c0e-1c05-4960-b750-0cb68c653ba2" 
+        },
+        { 
+            price: "$100K", 
+            actualPrice: "$529.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=02aee2af-0be9-48c0-9ff9-cbe5c42cc20a" 
+        },
+        { 
+            price: "$200K", 
+            actualPrice: "$989.00", 
+            features: {
+                "Profit Target": { phase1: "8%", phase2: "5%", funded: "-" },
+                "Duration": { phase1: "Unlimited", phase2: "Unlimited", funded: "Unlimited" },
+                "Minimum Trading Days": { phase1: "3 Days", phase2: "3 Days", funded: "-" },
+                "Max Daily Loss": { phase1: "5%", phase2: "5%", funded: "5%" },
+                "Max Loss": { phase1: "10%", phase2: "10%", funded: "10%" },
+                "Max Lots": { phase1: "-", phase2: "-", funded: "-" },
+                "Consistency Rule": { phase1: "No", phase2: "No", funded: "No" }
+            },
+            link: "https://prop.fundedx.com/buy-challenge/?id=746c6489-f8c8-4484-9793-05e21c6de92f" 
+        }
+    ]
+};
